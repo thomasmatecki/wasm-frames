e5ab7c8b7919963ff70296e5cfb9552e5a24b62b
thomas <thomas.matecki@gmail.com>
2020-02-29 12:22:35 -0500

    Assigning chunks

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ extern "C" {
 pub fn greet() {
     alert("Hello, wasm-frames!");
 }
+pub fn convolve_3x3(slice: &[u8]) {}
 
 #[wasm_bindgen]
 pub fn process_image(
@@ -29,20 +30,17 @@ pub fn process_image(
     width: f64,
     height: f64,
 ) {
+    utils::set_panic_hook();
+
     let image_data: Result<ImageData, JsValue> =
         source_context.get_image_data(0.0, 0.0, width, height);
 
     let input_pix: Clamped<Vec<u8>> = image_data.unwrap().data();
     let mut output_pix: Vec<u8> = vec![255; input_pix.len()];
     for (input_chunk, output_chunk) in input_pix.chunks(4).zip(output_pix.chunks_exact_mut(4)) {
-        let total: u16 = Iterator::sum(input_chunk[..2].iter().map(|i| *i as u16));
-        let mean = total / 3;
-        for i in output_chunk.iter_mut().take(3) {
-            *i = mean as u8;
-        }
+        let mean: u8 = Iterator::sum(input_chunk[..2].iter().map(|i| i / 3));
 
-        //output_chunk[0..2].clone_from_slice(&[mean as u8; 3]);
-        //log(&format!(" {:?}", output_chunk));
+        &output_chunk[0..3].copy_from_slice(&[mean; 3]);
     }
 
     let output = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut output_pix), 640, 480);
